7cbfe8f7c0a29380cd83375e20d526ffcf6988a7
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2011-06-03 16:27:18 +0000

    updated script for generate the strings for translation .po file
    git-svn-id: http://svn.plone.org/svn/collective/collective.amberjack.portlet/trunk@240398 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/amberjack/portlet/locales/update.sh
+++ collective/amberjack/portlet/locales/update.sh
@@ -1,5 +1,27 @@
+#i18ndude rebuild-pot --pot ./Content.pot \ 
+#                     --merge ../i18n/generated.pot \ 
+#                     --exclude=`find ../profiles -name "*.*py"` \
+#                     --create content ../ || exit 1
+
+rm ./rebuild_i18n.log
+
 i18ndude rebuild-pot --pot collective.amberjack.portlet.pot --create collective.amberjack.portlet --merge collective.amberjack.portlet-manual.pot ../
 i18ndude sync --pot collective.amberjack.portlet.pot ./*/LC_MESSAGES/collective.amberjack.portlet.po
 
 i18ndude rebuild-pot --pot ../i18n/collective.amberjack.portlet-plone.pot --create plone ../profiles
 i18ndude sync --pot ../i18n/collective.amberjack.portlet-plone.pot ../i18n/collective.amberjack.portlet-plone-*.po
+
+WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+touch ./rebuild_i18n.log
+
+find ../ -name "*pt" | xargs i18ndude find-untranslated > rebuild_i18n.log
